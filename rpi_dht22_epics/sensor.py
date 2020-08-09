ab033662dfc56a470e132aec4eae280cac10c62d
Pete R Jemian <prjemian@gmail.com>
2020-08-09 11:36:53 -0500

    MNT #7

--- rpi_dht22_epics/sensor.py
+++ rpi_dht22_epics/sensor.py
@@ -97,7 +97,7 @@ class DHT22_IOC(PVGroup):
 
     def __init__(self, *args, data_pin, update_period, **kwargs):
         super().__init__(*args, **kwargs)
-        self.device = adafruit_dht.DHT22(data_pin)
+        self.device = Adafruit_DHT.DHT22(data_pin)
         self.period = update_period
         self.smoothing = SMOOTHING_FACTOR
         self.trend_smoothing = 0.995    # reduces noise
