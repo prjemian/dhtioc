8b3b4b7bad99d653b0e931b33a6490ba7bad595a
Pete R Jemian <prjemian@gmail.com>
2020-08-16 01:49:26 -0500

    MNT #26

--- dhtioc/trend_analysis.py
+++ dhtioc/trend_analysis.py
@@ -24,7 +24,7 @@ class Trend:
 
     def __init__(self):
         """constructor"""
-        self.cache = {k: None for k in [0.8, 0.9, 0.95, 0.98, 0.99]}
+        self.cache = {k: None for k in [0.5, 0.7, 0.8, 0.9, 0.95]}
         self.stats = StatsRegClass()
         self.trend = None
         self._computed = False
