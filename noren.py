89f535fa8a757ce825ac413d5812174cfc9d24a9
Pete R Jemian <prjemian@gmail.com>
2020-08-15 23:48:43 -0500

    MNT #26

--- noren.py
+++ noren.py
@@ -1,14 +1,5 @@
 #!/usr/bin/env python3
 
-import board
-# from dhtioc import DHT_sensor, PIN, READ_PERIOD
 from dhtioc import reader
 
-
-def main():
-    # DHT_sensor(PIN, READ_PERIOD)
-    reader.main()
-
-
-if __name__ == "__main__":
-    main()
+reader.main()
