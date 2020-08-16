193c374bbf8f1458b2ecfe35fc185733e008bcd7
Pete R Jemian <prjemian@gmail.com>
2020-08-16 01:36:54 -0500

    MNT #26 syntax

--- dhtioc/ioc.py
+++ dhtioc/ioc.py
@@ -158,8 +158,7 @@ class MyIoc(PVGroup):
                 await self.temperature_f.write(value=C2F(self._temperature))
                 await self.temperature_trend.write(value=self._temperature_trend.slope)
 
-                c = self.counter.value
-                self.counter.write(value=c + 1)
+                await self.counter.write(value=c + self.counter.value)
 
             while time.time() < t_next_read:
                 await async_lib.library.sleep(INNER_LOOP_SLEEP)
