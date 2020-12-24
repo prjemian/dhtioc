97251f6075e986552fc804fb73b1df57a6a81007
Pete R. Jemian <prjemian@gmail.com>
2020-12-24 11:53:20 -0600

    WIP #45 Will print()be seen?

--- dhtioc/datalogger.py
+++ dhtioc/datalogger.py
@@ -38,7 +38,8 @@ class DataLogger:
     def __init__(self, ioc_prefix, path=None):
         """Constructor."""
         logger.setLevel("DEBUG")
-        logger.info(f"DataLogger starting for {ioc_prefix}")
+        logger.info("DataLogger starting for: ", ioc_prefix)
+        print(f"DataLogger starting for {ioc_prefix}")
         self.prefix = ioc_prefix
         self.base_path = path or os.path.abspath(
             os.path.join(
